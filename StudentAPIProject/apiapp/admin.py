cf9ddf1c8fd4151cb66eb924a86b90a0e27f994e
Shailesh Akarte <iamshael@gmail.com>
2019-12-14 01:32:01 +0530

    Adding Student model, migrations and migrate

--- StudentAPIProject/apiapp/admin.py
+++ StudentAPIProject/apiapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from apiapp.models import Student
 # Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["rollno", "name", "marks", "addr"]
+
+admin.site.register(Student,StudentAdmin)
